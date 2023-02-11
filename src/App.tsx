fa87a8d562475407c606f330cff6aa00a3d9b0c4
Dorota Leniec <dorleniec@gmail.com>
2023-02-11 17:50:11 +0100

    displaying notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import Note from "./components/Note";
+interface INote {
+  id: string;
+  body: string;
+}
 function App() {
+  const [notes, setNotes] = useState<INote[]>([]);
+
+  useEffect(() => {
+    const getNotes = async () => {
+      const res = await fetch("https://challenge.surfe.com/dorka/notes", {
+        method: "GET",
+      });
+      const data = await res.json();
+      console.log("data", data);
+      setNotes(data);
+    };
+
+    try {
+      console.log("getting notes");
+      getNotes();
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-900 p-5 flex gap-10 flex-wrap justify-center items-center text-white">
+      {notes.map((n) => (
+        <Note description={n.body} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Note.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+const Note = ({ description }: { description: string }) => {
+  const [note, setNote] = useState(description);
+  console.log("note", note);
+
+  const saveNote = () => {
+    const save = async () => {
+      fetch("https://challenge.surfe.com/dorka/notes", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({ body: note }),
+      });
+    };
+
+    try {
+      console.log("saving note");
+      save();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return (
+    <div className="p-2 rounded-lg w-60 h-60 border-1 border border-gray-400 bg-slate-100 text-gray-800 flex flex-col">
+      <label
+        htmlFor="note"
+        className="block text-sm font-medium text-gray-900 dark:text-white"
+      >
+        Note title
+      </label>
+      <p className="text-gray-600 text-xs">Last updated: 18 Apr, 2023</p>
+      <textarea
+        id="note"
+        rows={8}
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
+        className="mt-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 outline-none focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Type your curent thoughts..."
+      ></textarea>
+      <div>
+        <button
+          className="rounded px-5 py-2 mt-2 bg-red-500 text-wh
+        "
+          onClick={() => saveNote()}
+        >
+          Save note
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Note;
